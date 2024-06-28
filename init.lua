2b7e5a0f45cbf7f4253d36809332f963624c62ae
J MacPherson <jtdmacpherson@pinkieboi.com>
2024-06-28 23:33:14 +0100

    Initial commit

---
+++ init.lua
@@ -0,0 +1,23 @@
+-- [[ Install `lazy.nvim` plugin manager ]]
+--    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
+local lazypath = vim.fn.stdpath('data') .. '/lazy/lazy.nvim'
+if not vim.loop.fs_stat(lazypath) then
+    local lazyrepo = 'https://github.com/folke/lazy.nvim.git'
+    vim.fn.system({
+        'git',
+        'clone',
+        '--filter=blob:none',
+        '--branch=stable',
+        lazyrepo,
+        lazypath,
+    })
+end
+vim.opt.rtp:prepend(lazypath)
+
+vim.g.mapleader = ' '
+vim.g.maplocalleader = ' '
+
+require('vim-options')
+require('lazy').setup({
+    { import = 'plugins' },
+})
